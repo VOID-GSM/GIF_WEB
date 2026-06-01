6609de8be499bb4767395a22d8adaccc9a92dbe0
ma94275 <s25020@gsm.hs.kr>
2026-06-02 01:12:13 +0900

    chore: 공통 컴포넌트 index export 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,19 @@
-export { default as Button } from "./Button";
+export { default as NameBadge } from "./components/Badge/NameBadge";
+export { default as DeadlineBadge } from "./components/Badge/DeadlineBadge";
+export { default as StatusBadge } from "./components/Badge/StatusBadge";
+export { default as LabelBadge } from "./components/Badge/LabelBadge";
+export { default as SectionBadge } from "./components/Badge/SectionBadge";
+
+export { default as ScoreButton } from "./components/Button/ScoreButton";
+export { default as GrantButton } from "./components/Button/GrantButton";
+export { default as MemberButton } from "./components/Button/MemberButton";
+export { default as NextButton } from "./components/Button/NextButton";
+export { default as PageButton } from "./components/Button/PageButton";
+
+export { default as Input } from "./components/Input/Input";
+export { default as Textarea } from "./components/Input/Textarea";
+
+export { default as StyleDropdown } from "./components/Dropdown/StyleDropdown";
 
 export { default as Chevron } from "./svg/Chevron";
 export { default as Close } from "./svg/Close";
