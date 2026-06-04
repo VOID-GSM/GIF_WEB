cd7a52d367a8b9a94c17a8ee44b7b1aa8b359d47
ma94275 <s25020@gsm.hs.kr>
2026-06-04 20:52:44 +0900

    refactor: 공용 Navbar import로 변경

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Providers } from "./providers";
 import "./globals.css";
-import { Navbar } from "../components";
+import { Navbar } from "@repo/ui";
+import { CLIENT_NAV_ITEMS } from "../shared/constants/nav";
 
 export default function RootLayout({
   children,
@@ -9,9 +10,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>
-        <Navbar />
-        <Providers>{children}</Providers>
+      <body className="overflow-x-hidden h-screen">
+        <Navbar navItems={CLIENT_NAV_ITEMS} />
+
+        <main className="pt-20">
+          <Providers>{children}</Providers>
+        </main>
       </body>
     </html>
   );
