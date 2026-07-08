0f89c099a9d1dd48eb7fd6ba49f147efa926a770
jyuuuuu0 <s25023@gsm.hs.kr>
2026-07-08 12:23:46 +0900

    refactor: 프로젝트 로고 미등록/렌딩 실패 시 gif 로고 표시

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -22,6 +22,7 @@ export { default as NoticeButton } from "./components/Button/NoticeButton";
 export { default as Input } from "./components/Input/Input";
 export { default as Textarea } from "./components/Input/Textarea";
 export { default as FileUpload } from "./components/FileUpload/FileUpload";
+export { default as ProjectLogo } from "./components/ProjectLogo/ProjectLogo";
 
 export { default as StyleDropdown } from "./components/Dropdown/StyleDropdown";
 
